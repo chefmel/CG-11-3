f93f5198c34b789d3e1f47d1e4ca75c4731c7b2c
Melissa DoddRuesing <melissa.doddruesing@gmail.com>
2018-09-28 12:49:26 -0500

    CG 11-3

--- CG 11-3/Program.cs
+++ CG 11-3/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
-namespace CG_11_3
+namespace StudentNames
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Student> students = new List<Student>();
+
+            string line;
+            int numberOfGrades = 0;
+            StreamReader file = new StreamReader("C:\\Users\\melissa\\source\\repos\\StudentNames\\StudentNames\\TextFile1.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] lines = line.Split(" ");
+                numberOfGrades = lines.Length;
+                string studentName = lines[0];
+                Student newStudent = new Student(studentName, numberOfGrades);
+                students.Add(newStudent);
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    newStudent.AddScore(Convert.ToInt32(lines[i]));
+                }
+
+            }
+            foreach (Student item in students)
+            {
+                Console.WriteLine(item.Name + " " + item.MinGrade() + " " + item.MaxGrade());
+            }
+
+
+
+            Console.ReadLine();
         }
     }
 }
---
+++ CG 11-3/StudentMinMax.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+namespace StudentNames
+{
+    public class Student
+    {
+        public List<int> scores = new List<int>();
+        public string Name { get; set; }
+        public int Grades { get; set; }
+        public object Add { get; internal set; }
+
+        public Student(string name, int grades)
+        {
+            Name = name;
+            Grades = grades;
+        }
+        public void AddScore(int score)
+        {
+            scores.Add(score);
+        }
+        public int MinGrade()
+        {
+            return scores.Min();
+        }
+        public int MaxGrade()
+        {
+            return scores.Max();
+        }
+
+    }
+}
